c337b6e5356dfff59804f526d1e9dab52e147d33
Szabolcs Nagy <snagy@wcl.american.edu>
2019-02-16 07:39:36 -0500

    Updates

---
+++ precache-manifest.134d5c42030b67279eaea34830332028.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a18e7fee185ff4be5a46",
+    "url": "/react-hooks/static/js/runtime~main.a18e7fee.js"
+  },
+  {
+    "revision": "ed5e537cb090201e90ec",
+    "url": "/react-hooks/static/js/main.ed5e537c.chunk.js"
+  },
+  {
+    "revision": "4b89ba583e52ffdb66da",
+    "url": "/react-hooks/static/js/2.4b89ba58.chunk.js"
+  },
+  {
+    "revision": "ed5e537cb090201e90ec",
+    "url": "/react-hooks/static/css/main.7ac77eaa.chunk.css"
+  },
+  {
+    "revision": "cf21b6783b451788b0465a24455cf64a",
+    "url": "/react-hooks/index.html"
+  }
+];
